58968a648c2cca4892b2506d0603ff726aa284e8
Songzongyao <704140897@qq.com>
2018-04-13 09:27:49 +0800

    add a little comments for java class

--- StrutsLab/src/com/lab/utils/GetData.java
+++ StrutsLab/src/com/lab/utils/GetData.java
@@ -11,8 +11,42 @@ import com.lab.bean.Student;
 import com.lab.bean.Teacher;
 import com.lab.dao.DataBaseDao;
 
+
+/**
+ * 
+* Copyright: Copyright (c) 2018 songzongyao
+* 
+* @ClassName: GetData.java
+* @Description: 展示数据的时候获得数据的类，主要用于数据库中数据的展示。将数据库中某个表中的数据取出并以ArrayList的形式返回
+*
+* @version: v1.0.0
+* @author: songzongyao
+* @date: 2018年4月13日 上午9:17:18 
+*
+* Modification History:
+* Date         Author          Version                            Description
+*------------------------------------------------------------------------------------------*
+* 2018年4月13日     songzongyao           v1.0.0      新增getTeachers，getStudents，getSliderImages方法
+ */
 public class  GetData{
 	
+	/**
+	 * 
+	* @Function: GetData.java
+	* @Description: 获得数据库中teacher表中所有的数据，
+	* 
+	* @return：返回一个ArrayList<Teacher>
+	* @throws：异常描述
+	*
+	* @version: v1.0.0
+	* @author: songzongyao
+	* @date: 2018年4月13日 上午9:20:25 
+	*
+	* Modification History:
+	* Date         Author          Version            Description
+	*---------------------------------------------------------*
+	* 2018年4月13日     songzongyao           v1.0.0            新建函数
+	 */
 	public ArrayList<Teacher> getTeachers(){
 		ArrayList<Teacher> teachers = new ArrayList<Teacher>();
 		DataBaseDao db = new DataBaseDao();
@@ -35,11 +69,40 @@ public class  GetData{
 		}catch(SQLException e){
 			e.printStackTrace();
 		}
+		try {
+			rs.close();
+		} catch (SQLException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		try {
+			connection.close();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
 		return teachers;
 		
 	}
 	
+	/**
+	 * 
+	* @Function: GetData.java
+	* @Description: 获得数据库中student表中的所有数据
+	*
+	* @return：返回一个ArrayList<Student>
+	* @throws：异常描述
+	*
+	* @version: v1.0.0
+	* @author: songzongyao
+	* @date: 2018年4月13日 上午9:21:40 
+	*
+	* Modification History:
+	* Date         Author          Version            Description
+	*---------------------------------------------------------*
+	* 2018年4月13日     songzongyao           v1.0.0               新建函数
+	 */
 	public ArrayList<Student> getStudents(){
 		ArrayList<Student> students = new ArrayList<Student>();
 		DataBaseDao db = new DataBaseDao();
@@ -63,9 +126,38 @@ public class  GetData{
 		}catch(SQLException e){
 			e.printStackTrace();
 		}
+		try {
+			rs.close();
+		} catch (SQLException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		try {
+			connection.close();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		return students;
 	}
 	
+	/**
+	 * 
+	* @Function: GetData.java
+	* @Description: 获得数据库中sliderimage滑动图片的所有数据
+	*
+	* @return：返回一个ArrayList<SliderImage>
+	* @throws：异常描述
+	*
+	* @version: v1.0.0
+	* @author: songzongyao
+	* @date: 2018年4月13日 上午9:22:39 
+	*
+	* Modification History:
+	* Date         Author          Version            Description
+	*---------------------------------------------------------*
+	* 2018年4月13日     songzongyao           v1.0.0              新建函数
+	 */
 	public ArrayList<SliderImage> getSliderImages(){
 		ArrayList<SliderImage> sliderImages = new ArrayList<SliderImage>();
 		DataBaseDao db = new DataBaseDao();
@@ -87,6 +179,19 @@ public class  GetData{
 		}catch(SQLException e){
 			e.printStackTrace();
 		}
+		try {
+			rs.close();
+		} catch (SQLException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		try {
+			connection.close();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 		return sliderImages;
 	}
 	
