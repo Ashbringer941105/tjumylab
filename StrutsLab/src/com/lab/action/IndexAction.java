ba68c0593312f8545179cc9d12ed8763751df725
Songzongyao <704140897@qq.com>
2018-04-13 09:47:52 +0800

    give all action's comments

--- StrutsLab/src/com/lab/action/IndexAction.java
+++ StrutsLab/src/com/lab/action/IndexAction.java
@@ -7,6 +7,23 @@ import com.lab.utils.GetData;
 import com.opensymphony.xwork2.ActionContext;
 import com.opensymphony.xwork2.ActionSupport;
 
+/**
+ * 
+* Copyright: Copyright (c) 2018 songzongyao
+* 
+* @ClassName: IndexAction.java
+* @Description: 继承了ActionSupport类的action，响应index的action。
+* 从数据库中查询在index页面上需要展示的轮播图片，然后放到Session中
+*
+* @version: v1.0.0
+* @author: songzongyao
+* @date: 2018年4月13日 上午9:39:25 
+*
+* Modification History:
+* Date         Author          Version            Description
+*---------------------------------------------------------*
+* 2018年4月13日     songzongyao           v1.0.0               修改原因
+ */
 public class IndexAction extends ActionSupport {
 	public String execute() throws Exception {
 		// 获得ActionContext
